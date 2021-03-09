01d16c116079c7708d4ff4aa08e45d48be5c4fdc
Junjie <james9145@hotmail.com>
2021-03-09 11:30:21 +0800

    Completed calculator project

--- app.js
+++ app.js
@@ -1,38 +1,152 @@
 //dec var
 const display = document.querySelector('.display-txt');
+const displayOpp = document.querySelector('.opp-display');
 const numberButtons = document.querySelectorAll('.number-btn');
-const clearAll = document.querySelector('.clear');
-let displayNumber = '';
+const clearAllBtn = document.querySelector('.clear');
+const operateButtons = document.querySelectorAll('.opp');
+const equals = document.querySelector('.equals');
+const dec = document.querySelector('.dec');
+let currentOperation = null;
+let firstOperator = '';
+let secondOperator = '';
+let shouldResetScreen = false;
 
-
-
+//set operator
+function setOperator (operator) {
+    if (currentOperation !== null) evaluate()
+    firstOperator = display.textContent;
+    currentOperation = operator;  
+    shouldResetScreen = true;
+}
+operateButtons.forEach(button => {
+    button.addEventListener("click",() => setOperator(button.textContent));
+})
 
 //update display
-let numArray = [];
 numberButtons.forEach(button => { 
-    button.addEventListener('click', function() {  
-        numArray.push(button.textContent);
-        if (numArray.length >10) {
-            numArray.slice(0, 9);
-            return numArray;
-        }
-        displayNumber = numArray.join('');
-        display.textContent = displayNumber;
-        console.table(numArray);
-    })
+    button.addEventListener('click', () => appendNumber(button.textContent)) 
 })
+       
+function appendNumber (number) {
+    if(display.textContent === '0' ||shouldResetScreen) resetScreen();
+        display.textContent += number;
+    if (display.textContent.length = 17) {
+        display.textContent = display.textContent.toString().slice(0, 16)
+    }
+}
+
+//dec check
+function addDec() {
+    if(shouldResetScreen)resetScreen();
+    if (display.textContent === '') display.textContent = '0';
+    if (display.textContent.includes('.')) return;
+    display.textContent += '.'
+    }
+
+dec.addEventListener('click', addDec)
+
+//reset screen
+function resetScreen() {
+    display.textContent = '';
+    shouldResetScreen = false;
+}
+
 // delete button
 const deleteBtn = document.querySelector('.delete');
-deleteBtn.addEventListener('click', ()=>{
-    numArray = numArray.splice(-1, 1);
-    console.table(numArray);
-})
 
-
-//clear all function
-const clear = () => {
-    numArray = [];
-    displayNumber = '';
-    display.textContent = '';
+function deleteNum () {
+    display.textContent = display.textContent.toString().slice(0, -1);
 }
-clearAll.addEventListener('click', clear);
+
+deleteBtn.addEventListener('click', deleteNum)
+
+//clear all
+function clearAll () {
+    display.textContent = '0';
+    firstOperator = '';
+    secondOperator = '';
+    currentOperation = null;
+}
+
+clearAllBtn.addEventListener('click', clearAll);
+
+//calculate number
+function evaluate () {
+    if (currentOperation === null || shouldResetScreen) return;
+    if (currentOperation === "÷" && display.textContent === "0") {
+        alert("Cannot divide by zero");
+        clearAll();
+        return;
+        }
+    secondOperator = display.textContent;
+    console.log(firstOperator, currentOperation, secondOperator)
+    console.log(operate(currentOperation, firstOperator, secondOperator))
+    display.textContent = roundResult(operate(currentOperation, firstOperator, secondOperator))
+    currentOperation = null;
+    shouldResetScreen = true;
+    }
+
+function roundResult (number) {
+    return Math.round(number * 1000)/1000
+}
+equals.addEventListener('click', evaluate);
+
+//operators
+function addition (a, b) {
+   return a + b;
+}
+
+function subtraction (a, b) {
+    return a - b;
+}
+
+function multi (a, b) {
+    return a * b;
+}
+
+function division (a, b) {
+    return a/b;
+}
+
+function operate (operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch(operator) {
+        case '+':
+            return addition(a, b);
+            break;
+        case '-' :
+            return subtraction(a, b);
+            break;
+        case '×':
+            return multi(a, b);
+            break;
+        case '÷':
+            if (b === 0) return null;
+            else return a / b;
+            break;
+        default:
+            return null;
+    }
+}
+
+//keyboard funcionality
+function setInput (e) {
+    if(e.key >= 0 && e.key <= 9) appendNumber(e.key);
+    if(e.key === '.') addDec();
+    if(e.key === "=" || e.key ==="Enter") evaluate();
+    if(e.key === "Backspace") deleteNum();
+    if(e.key ==="Escape") clearAll();
+    if (e.key ==="+" || e.key === "-" || e.key ==="*" || e.key === "/") {
+        setOperator(oppKey(e.key))
+    }
+}
+
+function oppKey (opp) {
+    if (opp === "+") return "+";
+    if (opp === "-") return "-";
+    if (opp === "*") return "×";
+    if (opp === "/") return "÷";
+}
+
+window.addEventListener('keydown', setInput);
---
+++ readme.md
@@ -0,0 +1,7 @@
+This is a JS, HTML, and Css designed calculator built for The Odin Project.
+
+This is my first attempt at adding keyboard support to a web app, as well as,  my first use of SASS.
+
+Orginally I tried to use array methods to create the calculator, but I had difficulty getting the logic to work so I went with a much more basic approach.  
+
+I like to give thanks to github user michaelosman. Looking at his code helped me a lot when I got stuck. 
